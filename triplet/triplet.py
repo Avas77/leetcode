3373a4e21638a9058ce5e1682e61961ee9440e86
Avas <insanebuzz09@gmail.com>
2025-06-06 18:15:27 -0400

    found triplet sequence

---
+++ triplet/triplet.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        for i in range(len(nums)):
+            
+sol = Solution()
+print(sol.increasingTriplet([20,100,10,12,5,13]))
